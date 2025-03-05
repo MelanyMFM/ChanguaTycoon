df7a55b5b16458c9cb2e7b44b51c6df8bbd37e0d
Santiago Sosa <ssosag@unal.edu.co>
2025-03-05 06:20:16 -0500

    Mejora UI y funcionalidad compra cocina

--- Global.gd
+++ Global.gd
@@ -2,5 +2,5 @@ extends Node
 
 var dinero: int = 0
 var changuas_listas = 0
-var cps = 3
+var cps = 4
 var clientes_restantes: int = 2  # Número de clientes en el nivel
--- panel.gd
+++ panel.gd
@@ -6,9 +6,17 @@ extends Panel
 @onready var costo_label: Label = $Precio
 @onready var comprar_button: Button = $Button
 
+@onready var cocina = get_node("../../Fryer/Sketchfab_Scene")
+@export var costo_mejora_cocina: int = 100
+@export var aumento_cps_cocina: float = 0.015  
+
+@onready var costo_label_cocina: Label = $Precio2
+@onready var comprar_button_cocina: Button = $Button2
+
 @onready var efecto_sonido: AudioStreamPlayer3D = get_node("../../AudioStreamPlayer3D2")
 
 var max = 2
+var maxCocina = 1
 
 func _ready():
 	actualizar_ui()
@@ -16,6 +24,7 @@ func _ready():
 func actualizar_ui():
 	# Actualizar el Label con el costo de la mejora
 	costo_label.text = "Costo: $%d" % costo_mejora
+	costo_label_cocina.text = "Costo: $%d" % costo_mejora_cocina
 
 
 
@@ -36,6 +45,21 @@ func _on_button_pressed():
 	else:
 		Logger.log("No puedes mejorar más")
 
-
 func _on_texture_button_pressed():
 	visible = not visible  # Mostrar u ocultar la tienda
+
+
+func _on_button_2_pressed():
+	if maxCocina > 0:
+		if Global.dinero >= costo_mejora_cocina:
+			Global.dinero -= costo_mejora_cocina
+			cocina.visible = true
+			print(cocina)
+			actualizar_ui()
+			Logger.log("Mejora de cocina comprada!")
+			efecto_sonido.play()
+			maxCocina -= 1
+		else:
+			Logger.log("No tienes suficiente dinero para mejorar la cocina :(")
+	else:
+		Logger.log("No puedes mejorar la cocina más")
